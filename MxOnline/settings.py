ead0edc61402d9e6bede5caa0669ff4854b5c1d7
custertian <custertian@users.noreply.github.com>
2017-08-08 17:46:04 +0800

    10-1
    之前的内容完成了

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'xadmin',
     'crispy_forms',
     'captcha',
+    'pure_pagination',
 ]
 AUTH_USER_MODEL = "users.UserProfile"
 
@@ -77,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.media',
             ],
         },
     },
@@ -152,3 +154,5 @@ EMAIL_HOST_PASSWORD = "Value&1234"
 EMAIL_USE_TLS = False
 EMAIL_FROM = "custertian@sina.com"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """MxOnline URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,13 +15,18 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.static import serve
+
 import xadmin
 
 # from users.views import user_login
 from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView, ModifyPwdView
+from organization.views import OrgView
+from MxOnline.settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
@@ -35,4 +41,10 @@ urlpatterns = [
     url(r'^reset/(?P<active_code>.*)$', ResetView.as_view(), name="reset_pwd"),
     url(r'^modify_pwd/$', ModifyPwdView.as_view(), name="modify_pwd"),
 
+    # 课程机构url配置
+    url(r'^org/', include('organization.urls', namespace="org")),
+    # 课程相关url配置
+    url(r'^course/', include('courses.urls', namespace="course")),
+    # 配置上传文件的访问处理函数
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT})
 ]
